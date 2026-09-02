90fa660af9a711e96afc4a65c81bcac2bc22354d
Vinolia Moekwa <nmoekwa@byupathway.edu>
2026-09-02 11:53:19 +0200

    sentence Questions

---
+++ src/data/sentenceQuestions.js
@@ -0,0 +1,41 @@
+export const sentenceQuestions = [
+  {
+    words: ["The", "sun", "is", "hot"],
+  },
+  {
+    words: ["A", "dog", "can", "run"],
+  },
+  {
+    words: ["The", "pen", "is", "in", "the", "bag"],
+  },
+  {
+    words: ["The", "egg", "is", "in", "the", "bag"],
+  },
+  {
+    words: ["I", "see", "an", "owl"],
+  },
+  {
+    words: ["The", "key", "is", "in", "the", "bag"],
+  },
+  {
+    words: ["The", "fan", "is", "on"],
+  },
+  {
+    words: ["The", "map", "is", "in", "the", "bag"],
+  },
+  {
+    words: ["The", "web", "is", "big"],
+  },
+  {
+    words: ["The", "van", "is", "red"],
+  },
+  {
+    words: ["The", "net", "is", "big"],
+  },
+  {
+    words: ["The", "rug", "is", "red"],
+  },
+  {
+    words: ["The", "hat", "is", "big"],
+  },
+];
