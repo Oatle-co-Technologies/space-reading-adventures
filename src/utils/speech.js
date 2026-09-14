4fa1d53b013139211a8c64132483f06515015867
Vinolia Moekwa <nmoekwa@byupathway.edu>
2026-09-14 11:46:19 +0200

    TTS utils

---
+++ src/utils/speech.js
@@ -0,0 +1,15 @@
+export function speak(text) {
+  if (!("speechSynthesis" in window)) {
+    return;
+  }
+
+  window.speechSynthesis.cancel();
+
+  const utterance = new SpeechSynthesisUtterance(text);
+
+  utterance.rate = 0.85;
+  utterance.pitch = 1;
+  utterance.volume = 1;
+
+  window.speechSynthesis.speak(utterance);
+}
